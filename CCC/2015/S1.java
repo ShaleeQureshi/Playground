1828649cbbfbff9c7c2a78ef759eed09bdc1d874
ShaleeQureshi <shaleequreshi2019@gmail.com>
2021-01-20 20:52:57 -0500

    Changes to be committed:

---
+++ CCC/2015/S1.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class S1 {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int k = Integer.parseInt(reader.readLine());
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for (int i = 0; i < k; i++) {
+            int number = Integer.parseInt(reader.readLine());
+            if (number == 0) {
+                list.remove(list.size() - 1);
+            } else {
+                list.add(number);
+            }
+        }
+        System.out.println((int) list.stream().mapToDouble(a -> a).sum());
+
+    }
+}
